bc7493e963d769053fcebf63cfb5caad0bc344ed
Arrate Ortiz <arrate.ortizdezarate@roxall.es>
2021-06-23 13:03:09 +0200

    initial commit

---
+++ FluToDoApp/FluToDoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FluToDoApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FluToDoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FluToDoApp/FluToDoApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FluToDoApp.Models;
+using FluToDoApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FluToDoApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
